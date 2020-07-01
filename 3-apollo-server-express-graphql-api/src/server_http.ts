fadd0ab1f4bec32d6cd24c6abe051c63d2e65631
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2020-07-01 15:42:04 +0800

    server express

--- 3-apollo-server-express-graphql-api/src/server_http.ts
+++ 3-apollo-server-express-graphql-api/src/server_http.ts
@@ -7,13 +7,17 @@ import cors from "cors";
 import schema from "./schema";
 
 const app = express();
+
 const server = new ApolloServer({
   schema,
   validationRules: [depthLimit(7)],
 });
+
 app.use("*", cors());
 app.use(compression());
+
 server.applyMiddleware({ app, path: "/graphql" });
+
 const httpServer = createServer(app);
 httpServer.listen({ port: 3000 }, (): void =>
   console.log(`\n      GraphQL is now running on http://localhost:3000/graphql`)
