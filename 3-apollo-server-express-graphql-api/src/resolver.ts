8ba68f93f70dcd72e0fa52c54cb9e4d3a3265f64
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2020-07-01 17:10:20 +0800

    mutation example

--- 3-apollo-server-express-graphql-api/src/resolver.ts
+++ 3-apollo-server-express-graphql-api/src/resolver.ts
@@ -1,7 +1,30 @@
 import { IResolvers } from "graphql-tools";
+
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
+
+let idCount = links.length;
 const resolvers: IResolvers = {
   Query: {
     helloWorld: () => "Hello world from Apollo Server 3",
+  },
+  Mutation: {
+    // 2
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      console.log(links);
+      return link;
+    },
   },
 };
 export default resolvers;
