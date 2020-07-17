2b746a5e3cb584346b6bdcfc6fd2c46f69e1fd2c
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2020-07-17 15:43:56 +0800

    sls typescript

--- 3-apollo-server-lambda-serverless-api/dist/resolver.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var resolvers = {
-    Query: {
-        helloWorld: function () { return "Hello world from Apollo Server 3"; },
-    },
-};
-exports.default = resolvers;
