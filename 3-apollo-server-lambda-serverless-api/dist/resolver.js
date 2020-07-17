33fc1ed743236dbcf51f01f6022c1d92f44d25cd
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2020-07-17 12:42:04 +0800

    serverless example

---
+++ 3-apollo-server-lambda-serverless-api/dist/resolver.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var resolvers = {
+    Query: {
+        helloWorld: function () { return "Hello world from Apollo Server 3"; },
+    },
+};
+exports.default = resolvers;
