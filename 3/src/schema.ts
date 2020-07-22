d87d666b2451794fc272b79f931df1e4f7cbb536
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2020-07-22 14:13:47 +0800

    working now

--- 3/src/schema.ts
+++ 3/src/schema.ts
@@ -2,6 +2,7 @@ import { importSchema } from "graphql-import";
 import { makeExecutableSchema } from "graphql-tools";
 import resolvers from "./resolver";
 import { GraphQLSchema } from "graphql";
+
 const typeDefs = importSchema(process.cwd() + "/src/schema/schema.graphql");
 
 const schema: GraphQLSchema = makeExecutableSchema({
