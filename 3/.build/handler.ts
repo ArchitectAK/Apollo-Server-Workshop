6352ab0afccaf203b9a3ebe5220ed8692297f196
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2020-07-22 14:17:27 +0800

    optimized packages

--- 3/.build/handler.ts
+++ 3/.build/handler.ts
@@ -1,21 +1,5 @@
-import { Handler, Context, Callback } from "aws-lambda";
 import { server } from "./src/server";
 
-interface HelloResponse {
-  statusCode: number;
-  body: string;
-}
-
-const hello: Handler = (event: any, context: Context, callback: Callback) => {
-  const response: HelloResponse = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: Math.floor(Math.random() * 10),
-    }),
-  };
-
-  callback(undefined, response);
-};
 const graphqlHandler = server.createHandler();
 
-export { hello, graphqlHandler };
+export { graphqlHandler };
