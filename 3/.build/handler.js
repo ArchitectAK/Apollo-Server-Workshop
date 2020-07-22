67bd37eaba695fce66ca0faf9ae09fc44651cfe1
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2020-07-22 13:45:45 +0800

    optimized

--- 3/.build/handler.js
+++ 3/.build/handler.js
@@ -1,17 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.graphqlHandler = exports.hello = void 0;
+exports.graphqlHandler = void 0;
 const server_1 = require("./src/server");
-const hello = (event, context, callback) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: Math.floor(Math.random() * 10),
-        }),
-    };
-    callback(undefined, response);
-};
-exports.hello = hello;
 const graphqlHandler = server_1.server.createHandler();
 exports.graphqlHandler = graphqlHandler;
 //# sourceMappingURL=handler.js.map
