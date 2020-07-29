f8a6e4efa3d543cd5068f1b156181cc97bea29b0
Ankit Kumar <ankitdroiddeveloper@gmail.com>
2020-07-29 16:33:46 +0800

    Delete handler.js

--- 3/handler.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./src/server");
-const graphqlHandler = server_1.server.createHandler();
-exports.graphqlHandler = graphqlHandler;
-//# sourceMappingURL=handler.js.map
